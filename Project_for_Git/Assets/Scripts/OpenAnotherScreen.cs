03450d7181a5e67c5ff17f212ec5e70ecdc5af9a
Andrei Obrokov <obrokov@gmail.com>
2018-04-19 15:32:09 +0400

    test

---
+++ Project_for_Git/Assets/Scripts/OpenAnotherScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAnotherScreen : MonoBehaviour
+{
+	public GameObject ScreenToDicable;
+	public GameObject ScreenToEnable;
+
+	public void Open ()
+	{
+		ScreenToDicable.SetActive (false);
+		ScreenToEnable.SetActive (true);
+	}
+}
